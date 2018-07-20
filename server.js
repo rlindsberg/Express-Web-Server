cda7aec271bb171d150dd335372d823fbfd04dd1
Roderick Karlemstrand <roderick@karlemstrand.com>
2018-07-20 16:43:30 +0200

    Move project from Node.js repo here to push to Heroku

---
+++ server.js
@@ -0,0 +1,22 @@
+//express.js
+var express = require('express');
+var app = express();
+let port = process.env.PORT || 3000;
+//middleware
+var middleware = require('./middleware.js')
+
+
+app.use(middleware.logger); //app-level
+
+app.get('/about-us', middleware.requireAuthentication, function(req, res) {
+    res.send('About Us!')
+  }
+);
+
+//expose a folder
+app.use(express.static(__dirname + '/public_html')) //index.html is default
+
+
+app.listen(port, function() {
+    console.log('Express server listening at: 127.0.0.1:' + port);
+});
